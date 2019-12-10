5eb984a6d97ff161fe99eeef1caba654a1cfd549
215808 <215808@CARBON.CTI.P.LODZ.PL>
2019-12-10 14:44:55 +0100

    Stale - poprawa 3

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,6 +2,7 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
+    public static final int NO_VALUE = -1;
     private Node last;
 
     public void push(int i) {
@@ -24,13 +25,13 @@ public class IntLinkedList {
 
     public int top() {
         if (isEmpty())
-            return -1;
+            return NO_VALUE ;
         return last.getValue();
     }
 
     public int pop() {
         if (isEmpty())
-            return -1;
+            return NO_VALUE;
         int ret = last.getValue();
         last = last.getPrev();
         return ret;
